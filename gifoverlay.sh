9c7f252a0597f39c9499002492583c1e2693ed36
Quantroup <64614882+Quantroup@users.noreply.github.com>
2022-07-06 19:04:54 +0300

    Fix a bug / remove debug echos
    when cropping an image it cropped wrong (widthXwidth instead of widthXheight)
    
    now it's fixed

--- gifoverlay.sh
+++ gifoverlay.sh
@@ -33,12 +33,12 @@ while [ "$#" -gt "0" ]; do
 			#gifsize=`identify -ping -format '%w %h' ${1}[0]` 
 			#gifwidth=`identify -ping -format '%w' ${1}[0]`
 			#gifheight=`identify -ping -format '%h' ${1}[0]`
-			let gifarea=`identify -ping -format '%w*%h' ${1}[0]`
+			let gifarea=`identify -format '%w*%h' ${1}[0]`
 			
 			#imgsize=`identify -ping -format '%w %h' ${2}`
-			imgwidth=`identify -ping -format '%w' ${2}`
-			imgheight=`identify -ping -format '%h' ${2}`
-			let imgarea=`identify -ping -format '%w*%h' ${2}`
+			imgwidth=`identify -format '%w' ${2}`
+			imgheight=`identify -format '%h' ${2}`
+			let imgarea=`identify -format '%w*%h' ${2}`
 			
 			if [ ${gifarea} -gt ${imgarea} ]; then
 				filter="lanczos"
@@ -52,8 +52,8 @@ while [ "$#" -gt "0" ]; do
 				maskwidth=`magick identify -format '%w' ${mask}[${imgwidth}x${imgheight}]`
 				maskheight=`magick identify -format '%h' ${mask}[${imgwidth}x${imgheight}]`
 				let maskoffset="( ${imgwidth} / 2 ) - ( ${maskwidth} / 2 )"
-				echo "mask width: ${maskwidth}"
-				echo "mask offset: ${maskoffset}"
+				#echo "mask width: ${maskwidth}"
+				#echo "mask offset: ${maskoffset}"
 
 				if [ ${maskarea} -gt ${imgarea} ]; then
 					maskfilter="lanczos"
@@ -63,18 +63,19 @@ while [ "$#" -gt "0" ]; do
 
 
 				if [ ${imgarea} -eq ${maskarea} ]; then
-					convert \( \( ${1} -modulate 100,0,100 -coalesce \) null: ${2} -compose Overlay -layers composite \) -coalesce null: ${mask} -compose CopyOpacity -layers composite output.gif
+					convert \( \( ${1} -modulate 100,0,100 -coalesce \) null: ${2} -gravity west -compose Overlay -layers composite \) -coalesce null: ${mask} -gravity center -compose CopyOpacity -layers composite output.gif
 				else
-					convert \( \( \( ${1} -modulate 100,0,100 -coalesce \) -filter ${filter} -resize ${imgwidth}x${imgheight}^ -crop ${imgwidth}x${imgwidth}+0+0 +repage -coalesce \) null: ${2} -compose Overlay -layers composite \) null: \( ${mask} -filter ${maskfilter} -resize ${imgwidth}x${imgheight} +repage -coalesce \) -gravity center -compose CopyOpacity -layers composite -coalesce output.gif
-					mogrify -coalesce -crop ${maskwidth}x${imgheight}+${maskoffset}+0 output.gif
+					convert \( \( \( ${1} -modulate 100,0,100 -coalesce \) -gravity west -filter ${filter} -resize ${imgwidth}x${imgheight}^ -crop ${imgwidth}x${imgheight}+0+0 +repage -coalesce \) null: ${2} -compose Overlay -layers composite \) null: \( ${mask} -filter ${maskfilter} -resize ${imgwidth}x${imgheight} +repage -coalesce \) -gravity center -compose CopyOpacity -layers composite -coalesce output.gif
+					mogrify -gravity west -coalesce -crop ${maskwidth}x${maskheight}+${maskoffset}+0 output.gif
 				fi
 				
 			else
 				if [ ${gifarea} -eq ${imgarea} ]; then
-					convert \( ${1} -modulate 100,0,100 -coalesce \) null: ${2} -compose Overlay -layers composite -layers optimize output.gif
+					convert \( ${1} -modulate 100,0,100 -coalesce \) null: ${2} -gravity west -compose Overlay -layers composite -layers optimize output.gif
 				
 				else
-					convert \( \( ${1} -modulate 100,0,100 -coalesce \) -filter ${filter} -resize ${imgwidth}x${imgheight}^ -crop ${imgwidth}x${imgwidth}+0+0 +repage -coalesce \) null: ${2} -compose Overlay -layers composite -layers optimize output.gif
+					convert \( \( ${1} -modulate 100,0,100 -coalesce \) -filter ${filter} -resize ${imgwidth}x${imgheight}^ -crop ${imgwidth}x${imgheight}+0+0 +repage -coalesce \) null: ${2} -gravity west -compose Overlay -layers composite -layers optimize output.gif
+				
 				
 				fi
 			fi
