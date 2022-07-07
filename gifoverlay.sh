f75a847e6bab463b91835c0b7aaade77771b9f61
Quantroup <64614882+Quantroup@users.noreply.github.com>
2022-07-07 12:22:52 +0300

    Refactor code / fix errors
    refactor the main loop (as in the actual imagemagick command) into many calls to imagemagick
    this makes adding new features easier.
    Also we made handling of dimentions a bit better. (although it might still be a problem if the aspect ratios and the area of the files are wonky / same)
    there appears to be no regressions but the old script is also uploaded if the need arises.

--- gifoverlay.sh
+++ gifoverlay.sh
@@ -2,7 +2,9 @@
 
 # gifoverlay.sh: a tool to overlay pictures on top of a possibly tiling gif (TODO)
 
-TILE=0
+tile=0
+optimize=0
+filename="output.gif"
 mask="none"
 
 while [ "$#" -gt "0" ]; do
@@ -13,12 +15,14 @@ while [ "$#" -gt "0" ]; do
 			echo "usage: gifoverlay.sh [options] [gif to use as template] [image to overlay]"
 			echo "Options available:"
 			echo "-h , --help : lists this help"
-			echo "-t , --tile : tile the gif to fit the picture (TODO!)"
+			echo "-t , --tile : tile the gif to fill (aka make sure the gif covers the entire thing) the image then crop to image"
 			echo "-m=foo , --mask=foo : use the file foo for a mask (this copies the alpha channel of the template) (thanks to @piconaut for help!)"
+			echo "-o , --optimize : optimize the gif layers."
+			echo "-f=bar , --file=bar : save to the file named bar"
 		;;
 		
 		-t | --tile)
-			TILE=1
+			tile=1
 		;;
 
 		-m=* | --mask=* )
@@ -27,15 +31,24 @@ while [ "$#" -gt "0" ]; do
 			
 		;;
 
+		-o | --optimize )
+			optimize=1
+		;;
+
+		-f=* | --file=* )
+			filename="`echo ${1} | cut -d '=' -f 2`"
+			echo "Filename to save as: ${filename}"
+		;;
+
 		*) #done reading options, assume the rest are file names.
 			
 			#just get start frame
-			#gifsize=`identify -ping -format '%w %h' ${1}[0]` 
-			#gifwidth=`identify -ping -format '%w' ${1}[0]`
-			#gifheight=`identify -ping -format '%h' ${1}[0]`
+			gifsize=`identify -ping -format '%w%h' ${1}[0]` 
+			gifwidth=`identify -ping -format '%w' ${1}[0]`
+			gifheight=`identify -ping -format '%h' ${1}[0]`
 			let gifarea=`identify -format '%w*%h' ${1}[0]`
 			
-			#imgsize=`identify -ping -format '%w %h' ${2}`
+			imgsize=`identify -ping -format '%w%h' ${2}`
 			imgwidth=`identify -format '%w' ${2}`
 			imgheight=`identify -format '%h' ${2}`
 			let imgarea=`identify -format '%w*%h' ${2}`
@@ -48,6 +61,7 @@ while [ "$#" -gt "0" ]; do
 			
 			if [ ${mask} != "none" ]; then
 				
+				masksize=`identify -ping -format '%w%h' ${mask}`
 				let maskarea=`identify -ping -format '%w*%h' ${mask}`
 				maskwidth=`magick identify -format '%w' ${mask}[${imgwidth}x${imgheight}]`
 				maskheight=`magick identify -format '%h' ${mask}[${imgwidth}x${imgheight}]`
@@ -55,29 +69,45 @@ while [ "$#" -gt "0" ]; do
 				#echo "mask width: ${maskwidth}"
 				#echo "mask offset: ${maskoffset}"
 
-				if [ ${maskarea} -gt ${imgarea} ]; then
+				if [ ${masksize} -gt ${imgsize} ]; then
 					maskfilter="lanczos"
 				else
 					maskfilter="point"
 				fi
-
-
-				if [ ${imgarea} -eq ${maskarea} ]; then
-					convert \( \( ${1} -modulate 100,0,100 -coalesce \) null: ${2} -gravity west -compose Overlay -layers composite \) -coalesce null: ${mask} -gravity center -compose CopyOpacity -layers composite output.gif
-				else
-					convert \( \( \( ${1} -modulate 100,0,100 -coalesce \) -gravity west -filter ${filter} -resize ${imgwidth}x${imgheight}^ -crop ${imgwidth}x${imgheight}+0+0 +repage -coalesce \) null: ${2} -compose Overlay -layers composite \) null: \( ${mask} -filter ${maskfilter} -resize ${imgwidth}x${imgheight} +repage -coalesce \) -gravity center -compose CopyOpacity -layers composite -coalesce output.gif
-					mogrify -gravity west -coalesce -crop ${maskwidth}x${maskheight}+${maskoffset}+0 output.gif
-				fi
-				
+			fi
+			
+			# b&w gif
+			magick ${1} -modulate 100,0,100 -coalesce tempout.gif
+			
+			# handle size difference between gif and img (and tiling)
+			if [ ${tile} -eq 1 ]; then
+				echo "+++++++ TODO! sorry... but we must end the program here (or it will fail horribly) +++++++"
+				exit 1
+			elif [ ${gifsize} -eq ${imgsize} ]; then
+				: #do nothing.
 			else
-				if [ ${gifarea} -eq ${imgarea} ]; then
-					convert \( ${1} -modulate 100,0,100 -coalesce \) null: ${2} -gravity west -compose Overlay -layers composite -layers optimize output.gif
-				
+				magick mogrify -gravity west -filter ${filter} -resize ${imgwidth}x${imgheight}^ -crop ${imgwidth}x${imgheight}+0+0 +repage -coalesce tempout.gif
+			fi
+
+			# overlay image ontop of the gif
+			magick tempout.gif  null: ${2} -gravity west -compose Overlay -layers composite tempout.gif
+
+			# process mask
+			if [ ${mask} = "none" ]; then
+				: #nothing. wait for optimize
+			else
+				if [ ${imgsize} -eq ${masksize} ]; then
+					magick tempout.gif -coalesce null: ${mask} -gravity center -compose CopyOpacity -layers tempout.gif
 				else
-					convert \( \( ${1} -modulate 100,0,100 -coalesce \) -filter ${filter} -resize ${imgwidth}x${imgheight}^ -crop ${imgwidth}x${imgheight}+0+0 +repage -coalesce \) null: ${2} -gravity west -compose Overlay -layers composite -layers optimize output.gif
-				
-				
+					magick tempout.gif -coalesce null: \( ${mask} -filter ${maskfilter} -resize ${imgwidth}x${imgheight} +repage -coalesce \) -gravity center -compose CopyOpacity -layers composite -coalesce tempout.gif
+					mogrify tempout.gif -gravity west -coalesce -crop ${maskwidth}x${maskheight}+${maskoffset}+0 tempout.gif
 				fi
+			fi
+			
+			if [ ${optimize} -eq 1 ]; then
+				magick tempout.gif -layers optimize ${filename}
+			else
+				mv tempout.gif ${filename}
 			fi
 
 			break
---
+++ oldscript.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+
+# gifoverlay.sh: a tool to overlay pictures on top of a possibly tiling gif (TODO)
+
+TILE=0
+mask="none"
+
+while [ "$#" -gt "0" ]; do
+	case $1 in
+		-h | --help)
+			echo "- gifoverlay.sh -"
+			echo "A tool for overlaying stuff in gifs"
+			echo "usage: gifoverlay.sh [options] [gif to use as template] [image to overlay]"
+			echo "Options available:"
+			echo "-h , --help : lists this help"
+			echo "-t , --tile : tile the gif to fit the picture (TODO!)"
+			echo "-m=foo , --mask=foo : use the file foo for a mask (this copies the alpha channel of the template) (thanks to @piconaut for help!)"
+		;;
+		
+		-t | --tile)
+			TILE=1
+		;;
+
+		-m=* | --mask=* )
+			mask="`echo ${1} | cut -d '=' -f 2`"
+			echo "Mask file: ${mask}"
+			
+		;;
+
+		*) #done reading options, assume the rest are file names.
+			
+			#just get start frame
+			#gifsize=`identify -ping -format '%w %h' ${1}[0]` 
+			#gifwidth=`identify -ping -format '%w' ${1}[0]`
+			#gifheight=`identify -ping -format '%h' ${1}[0]`
+			let gifarea=`identify -format '%w*%h' ${1}[0]`
+			
+			#imgsize=`identify -ping -format '%w %h' ${2}`
+			imgwidth=`identify -format '%w' ${2}`
+			imgheight=`identify -format '%h' ${2}`
+			let imgarea=`identify -format '%w*%h' ${2}`
+			
+			if [ ${gifarea} -gt ${imgarea} ]; then
+				filter="lanczos"
+			else
+				filter="point"
+			fi
+			
+			if [ ${mask} != "none" ]; then
+				
+				let maskarea=`identify -ping -format '%w*%h' ${mask}`
+				maskwidth=`magick identify -format '%w' ${mask}[${imgwidth}x${imgheight}]`
+				maskheight=`magick identify -format '%h' ${mask}[${imgwidth}x${imgheight}]`
+				let maskoffset="( ${imgwidth} / 2 ) - ( ${maskwidth} / 2 )"
+				#echo "mask width: ${maskwidth}"
+				#echo "mask offset: ${maskoffset}"
+
+				if [ ${maskarea} -gt ${imgarea} ]; then
+					maskfilter="lanczos"
+				else
+					maskfilter="point"
+				fi
+
+
+				if [ ${imgarea} -eq ${maskarea} ]; then
+					convert \( \( ${1} -modulate 100,0,100 -coalesce \) null: ${2} -gravity west -compose Overlay -layers composite \) -coalesce null: ${mask} -gravity center -compose CopyOpacity -layers composite output.gif
+				else
+					convert \( \( \( ${1} -modulate 100,0,100 -coalesce \) -gravity west -filter ${filter} -resize ${imgwidth}x${imgheight}^ -crop ${imgwidth}x${imgheight}+0+0 +repage -coalesce \) null: ${2} -compose Overlay -layers composite \) null: \( ${mask} -filter ${maskfilter} -resize ${imgwidth}x${imgheight} +repage -coalesce \) -gravity center -compose CopyOpacity -layers composite -coalesce output.gif
+					mogrify -gravity west -coalesce -crop ${maskwidth}x${maskheight}+${maskoffset}+0 output.gif
+				fi
+				
+			else
+				if [ ${gifarea} -eq ${imgarea} ]; then
+					convert \( ${1} -modulate 100,0,100 -coalesce \) null: ${2} -gravity west -compose Overlay -layers composite -layers optimize output.gif
+				
+				else
+					convert \( \( ${1} -modulate 100,0,100 -coalesce \) -filter ${filter} -resize ${imgwidth}x${imgheight}^ -crop ${imgwidth}x${imgheight}+0+0 +repage -coalesce \) null: ${2} -gravity west -compose Overlay -layers composite -layers optimize output.gif
+				
+				
+				fi
+			fi
+
+			break
+		;;
+	esac
+	shift
+done
